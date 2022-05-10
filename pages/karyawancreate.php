6a57d266de99dc88bc42a36c1607e2fc1139abca
mirzayogy <mirza.yogy@gmail.com>
2022-05-10 13:55:06 +0800

    Redirect setelah simpan

--- pages/karyawancreate.php
+++ pages/karyawancreate.php
@@ -24,6 +24,8 @@
                 $statement->bindParam(3, $jenis_kelamin);
                 $statement->bindParam(4, $status_menikah);
                 $statement->execute();
+                
+                header('Location: main.php?page=karyawan');
             }
 
             ?>
