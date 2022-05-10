da4f8837d840964284dda8698760a0d70453e3ea
mirzayogy <mirza.yogy@gmail.com>
2022-05-10 14:03:12 +0800

    input dengan radiogroup

--- pages/karyawancreate.php
+++ pages/karyawancreate.php
@@ -2,17 +2,17 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Tambah Karyawan</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-            <?php 
+            <?php
             include_once "../database/database.php";
-            
-            if(isset($_POST['button_simpan'])){
+
+            if (isset($_POST['button_simpan'])) {
                 $nik = $_POST['nik'];
                 $nama_karyawan = $_POST['nama_karyawan'];
                 $jenis_kelamin = $_POST['jenis_kelamin'];
                 $status_menikah = $_POST['status_menikah'];
 
-                // $insertSQL = "INSERT INTO karyawan VALUES (NULL,'" . $nik . "','" . $nama_karyawan . "','"
-                //     . $jenis_kelamin . "','" . $status_menikah . "')";
+                $insertSQL = "INSERT INTO karyawan VALUES (NULL,'" . $nik . "','" . $nama_karyawan . "','"
+                    . $jenis_kelamin . "','" . $status_menikah . "')";
 
                 $insertSQL = "INSERT INTO karyawan VALUES (NULL, ?, ?, ?, ?)";
 
@@ -24,7 +24,7 @@
                 $statement->bindParam(3, $jenis_kelamin);
                 $statement->bindParam(4, $status_menikah);
                 $statement->execute();
-                
+
                 header('Location: main.php?page=karyawan');
             }
 
@@ -41,9 +41,20 @@
                 <label for="nama_karyawan" class="form-label">Nama Karyawan</label>
                 <input type="text" class="form-control" id="nama_karyawan" name="nama_karyawan">
             </div>
-            <div class="mb-3">
+            <div>
                 <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
-                <input type="text" class="form-control" id="jenis_kelamin" name="jenis_kelamin">
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" value="Laki-laki" name="jenis_kelamin" id="jenis_kelamin1">
+                <label class="form-check-label" for="jenis_kelamin1">
+                    Laki-laki
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" value="Perempuan" name="jenis_kelamin" id="jenis_kelamin2">
+                <label class="form-check-label" for="jenis_kelamin2">
+                    Perempuan
+                </label>
             </div>
             <div class="mb-3">
                 <label for="status_menikah" class="form-label">Status Menikah</label>
