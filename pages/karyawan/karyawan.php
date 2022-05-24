1b96052a3f28c40e115318ada2bfa0e015cf0587
mirzayogy <mirza.yogy@gmail.com>
2022-05-24 13:33:26 +0800

    pertemuan 4

--- pages/karyawan/karyawan.php
+++ pages/karyawan/karyawan.php
@@ -35,12 +35,13 @@
             <td><?php echo $data['nama_karyawan'] ?></td>
             <td><?php echo $data['jenis_kelamin'] ?></td>
             <td>
-              <button class="btn btn-warning btn-sm">
-                <span data-feather="clipboard"></span>
-              </button>
-              <button class="btn btn-danger btn-sm">
-                <span data-feather="x-octagon"></span>
-              </button>
+              <a href="?page=karyawanupdate&id=<?php echo $data['id'] ?>" class="badge bg-warning">
+                <span data-feather="edit">
+              </a>
+              <a href="?page=karyawandelete&id=<?php echo $data['id'] ?>" class="badge bg-danger">
+                <span data-feather="x-octagon">
+              </a>
+
             </td>
           </tr>
         <?php
